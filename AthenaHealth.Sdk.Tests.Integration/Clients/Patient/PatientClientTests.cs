a37fc9ba2e613e9c86b1ed5d7163afb274149090
dawid-heftowicz <dawid.heftowicz@htdevelopers.com>
2019-10-16 10:24:42 +0200

    ANL-21 Get patient's default pharmacy

---
+++ AthenaHealth.Sdk.Tests.Integration/Clients/Patient/PatientClientTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AthenaHealth.Sdk.Exceptions;
+using AthenaHealth.Sdk.Tests.Integration.TestingHelpers;
+using Shouldly;
+using Xunit;
+// ReSharper disable StringLiteralTypo
+
+namespace AthenaHealth.Sdk.Tests.Integration.Clients.Patient
+{
+    public class PatientClientTests
+    {
+        [Fact]
+        public async Task GetPatientById_ValidId_ReturnsPatient ()
+        {
+            var patientClient = new Sdk.Clients.PatientClient(ConnectionFactory.CreateFromFile(@"Clients\Patient\Patient.json"));
+
+            Models.Response.Patient patient = await patientClient.GetPatientById(1);
+
+            patient.ShouldNotBeNull();
+            patient.Email.ShouldBe("monroe86@hotmail.com");
+            patient.DepartmentId.ShouldBe(162);
+            patient.Balances.ShouldNotBeEmpty();
+            patient.Balances.First().Value.ShouldBe(10);
+        }
+
+        [Fact]
+        public void GetPatientById_InvalidId_ThrowsApiException ()
+        {
+            var patientClient = new Sdk.Clients.PatientClient(ConnectionFactory.CreateFromFile(@"Clients\Patient\GetPatient_InvalidId.json", HttpStatusCode.OK)); //In this case athena respond with HTTP 200 OK and status code 400 in response body
+            ApiException exc = Should.Throw<ApiException>(async () => await patientClient.GetPatientById(0));
+            exc.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task GetDefaultPharmacy_ValidId_ReturnsDefaultPharmacy ()
+        {
+            var patientClient = new Sdk.Clients.PatientClient(ConnectionFactory.CreateFromFile(@"Clients\Patient\GetDefaultPharmacy.json", HttpStatusCode.OK));
+            
+            Models.Response.Pharmacy pharmacy = await patientClient.GetDefaultPharmacy(195900, 300, 1);
+
+            pharmacy.ShouldNotBeNull();
+            pharmacy.State.ShouldBe("NY");
+            pharmacy.ClinicalProviderName.ShouldBe("Himani Shishodia");
+        }
+    }
+}
---
+++ AthenaHealth.Sdk/Models/Response/Pharmacy.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+// ReSharper disable StringLiteralTypo
+// ReSharper disable CommentTypo
+
+namespace AthenaHealth.Sdk.Models.Response
+{
+    public class Pharmacy
+    {
+        /// <summary>
+        /// The athenaNet clinical provider ID.
+        /// </summary>
+        [JsonProperty(PropertyName = "clinicalproviderid")]
+        public int ClinicalProviderId { get; set; }
+
+        /// <summary>
+        /// The pharmacy name
+        /// </summary>
+        [JsonProperty(PropertyName = "clinicalprovidername")]
+        public string ClinicalProviderName { get; set; }
+
+        /// <summary>
+        /// Address line 1 of the pharmacy
+        /// </summary>
+        [JsonProperty(PropertyName = "address1")]
+        public string Address1 { get; set; }
+
+        /// <summary>
+        /// Address line 2 of the pharmacy
+        /// </summary>
+        [JsonProperty(PropertyName = "address2")]
+        public string Address2 { get; set; }
+
+        /// <summary>
+        /// City of the pharmacy
+        /// </summary>
+        [JsonProperty(PropertyName = "city")]
+        public string City { get; set; }
+
+        /// <summary>
+        /// State of the pharmacy
+        /// </summary>
+        [JsonProperty(PropertyName = "state")]
+        public string State { get; set; }
+
+        /// <summary>
+        /// Zip code of the pharmacy
+        /// </summary>
+        [JsonProperty(PropertyName = "zip")]
+        public string Zip { get; set; }
+
+        /// <summary>
+        /// Phone number of the pharmacy
+        /// </summary>
+        [JsonProperty(PropertyName = "phonenumber")]
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Fax number of the pharmacy
+        /// </summary>
+        [JsonProperty(PropertyName = "faxnumber")]
+        public string FaxNumber { get; set; }
+
+        /// <summary>
+        /// Flag representing if the pharmacy accepts fax
+        /// </summary>
+        [JsonProperty(PropertyName = "acceptfax")]
+        public bool AcceptFax { get; set; }
+
+        /// <summary>
+        /// Type of pharmacy (retail, mailorder)
+        /// </summary>
+        [JsonProperty(PropertyName = "pharmacytype")]
+        public string PharmacyType { get; set; }
+
+        /// <summary>
+        /// Receiver type
+        /// </summary>
+        [JsonProperty(PropertyName = "receivertype")]
+        public string ReceiverType { get; set; }
+
+        /// <summary>
+        /// If flagged true, this is the default pharmacy for the patient
+        /// </summary>
+        [JsonProperty(PropertyName = "defaultpharmacy")]
+        public bool DefaultPharmacy { get; set; }
+
+
+
+    }
+}
