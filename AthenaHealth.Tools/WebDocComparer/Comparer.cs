c03d4865bac4312ae9e00cfa15b26c59dfe125cd
dawid-heftowicz <dawid.heftowicz@htdevelopers.com>
2019-10-21 15:01:46 +0200

    WebDocParser tuned and Comparer added

---
+++ AthenaHealth.Tools/WebDocComparer/Comparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AthenaHealth.Tools.WebDocParser;
+
+namespace AthenaHealth.Tools.WebDocComparer
+{
+    public static class Comparer
+    {
+        public static void Compare()
+        {
+            var in1 = Parser.ParseItems("WebDocComparer\\in1.html");
+            var in2 = Parser.ParseItems("WebDocComparer\\in2.html");
+
+            Console.WriteLine("Scanning in1 against in2...");
+            foreach (var item in in1)
+            {
+                if (!in2.Contains(item))
+                    Console.WriteLine($"{item.Name} ({item.Type}) exists in in1.html, but not exists in in2.html");
+            }
+
+            Console.WriteLine("Scanning in2 against in1...");
+            foreach (var item in in1)
+            {
+                if (!in2.Contains(item))
+                    Console.WriteLine($"{item.Name} ({item.Type}) exists in in1.html, but not exists in in2.html");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
