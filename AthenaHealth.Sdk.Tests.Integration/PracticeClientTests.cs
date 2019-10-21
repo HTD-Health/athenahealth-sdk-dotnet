d52f730d08d2f8ee53c4d580c3696125253481d7
Bartlomiej Marek <bartlomiej.mare@htdevelopers.com>
2019-10-21 08:01:55 +0200

    ANL-96 Practice & Provider integration tests

---
+++ AthenaHealth.Sdk.Tests.Integration/PracticeClientTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AthenaHealth.Sdk.Tests.Integration.TestingHelpers;
+using Shouldly;
+using Xunit;
+
+// ReSharper disable StringLiteralTypo
+namespace AthenaHealth.Sdk.Tests.Integration
+{
+    public class PracticeClientTests
+    {
+        [Fact]
+        public async Task GetAll_ReturnsRecords()
+        {
+            var client = new Sdk.Clients.PracticeClient(ConnectionFactory.CreateFromFile(@"Data\Practice\GetAll.json"));
+
+            var response = await client.GetAll();
+
+            response.ShouldNotBeNull();
+            response.Total.ShouldBeGreaterThan(0);
+            response.Items.Any(x => x.Name == "athenahealth MDP Sandbox").ShouldBeTrue();
+            response.Items.Any(x => x.Id == "195900").ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task GetById_ExistingId_ReturnsRecord()
+        {
+            var client = new Sdk.Clients.PracticeClient(ConnectionFactory.CreateFromFile(@"Data\Practice\GetById.json"));
+
+            var response = await client.GetById(195900);
+
+            response.ShouldNotBeNull();
+            response.Id.ShouldBe("195900");
+            response.Name.ShouldBe("athenahealth MDP Sandbox");
+        }
+    }
+}
---
+++ AthenaHealth.Sdk.Tests.Integration/ProviderClientTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AthenaHealth.Sdk.Tests.Integration.TestingHelpers;
+using Shouldly;
+using Xunit;
+
+namespace AthenaHealth.Sdk.Tests.Integration
+{
+    public class ProviderClientTests
+    {
+        [Fact]
+        public async Task GetAll_ReturnsRecords()
+        {
+            var client = new Sdk.Clients.ProviderClient(ConnectionFactory.CreateFromFile(@"Data\Provider\GetAll.json"));
+
+            var response = await client.GetAll();
+
+            response.ShouldNotBeNull();
+            response.Total.ShouldBeGreaterThan(0);
+            response.Items.Any(x => x.FirstName == "Terry").ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task GetById_ExistingId_ReturnsRecord()
+        {
+            var client = new Sdk.Clients.ProviderClient(ConnectionFactory.CreateFromFile(@"Data\Provider\GetById.json"));
+
+            var response = await client.GetById(86);
+
+            response.ShouldNotBeNull();
+            response.Id.ShouldBe("86");
+            response.FirstName.ShouldBe("Terry");
+        }
+    }
+}
