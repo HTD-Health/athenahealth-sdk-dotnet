68152b6694ecef028e2feeb36ba43e1190c0549b
dawid.kraczkowski@htdevelopers.com <dawid.kraczkowski@htdevelopers.com>
2019-10-29 13:16:04 +0100

    CheckInField converter

---
+++ AthenaHealth.Sdk.Tests/Models/Converters/SingleElementToArrayConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using AthenaHealth.Sdk.Models.Converters;
+using Newtonsoft.Json;
+using Shouldly;
+using Xunit;
+
+namespace AthenaHealth.Sdk.Tests.Models.Converters
+{
+    public class SingleElementToArrayConverterTests
+    {
+        [Fact]
+        public void SingleElementTest()
+        {
+            // Arrange
+            var json = "{\"array\":{\"property\":12}}";
+
+            // Act
+            var result = JsonConvert.DeserializeObject<Model>(json);
+
+            // Assert
+            result.Array.Length.ShouldBe(1);
+            result.Array[0].Property.ShouldBe(12);
+        }
+
+        [Fact]
+        public void MultiElementTest()
+        {
+            // Arrange
+            var json = "{\"array\":[{\"property\":12},{\"property\":15}]}";
+
+            // Act
+            var result = JsonConvert.DeserializeObject<Model>(json);
+
+            // Assert
+            result.Array.Length.ShouldBe(2);
+            result.Array[0].Property.ShouldBe(12);
+            result.Array[1].Property.ShouldBe(15);
+        }
+
+        public class Model
+        {
+            [JsonProperty("array")]
+            [JsonConverter(typeof(SingleElementToArrayConverter), typeof(InnerModel))]
+            public InnerModel[] Array { get; set; }
+
+            public class InnerModel
+            {
+                [JsonProperty("property")]
+                public int Property { get; set; }
+            }
+        }
+    }
+}
---
+++ AthenaHealth.Sdk/Models/Converters/SingleElementToArrayConverter.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace AthenaHealth.Sdk.Models.Converters
+{
+    public class SingleElementToArrayConverter : JsonConverter
+    {
+        private readonly Type _type;
+
+        public override bool CanRead => true;
+
+        public override bool CanWrite => false;
+
+        public SingleElementToArrayConverter(Type type)
+        {
+            _type = type;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JToken jToken = JToken.Load(reader);
+
+            
+            if (jToken.Type == JTokenType.Object)
+            {
+                // In case of single object we have to create array and populate it
+                JObject jObject = jToken as JObject;
+
+                object target = Activator.CreateInstance(_type);
+
+                using (JsonReader jObjectReader = CopyReaderForObject(reader, jObject))
+                {
+                    serializer.Populate(jObjectReader, target);
+                }
+
+                Array array = Array.CreateInstance(_type, 1);
+
+                array.SetValue(target, 0);
+
+                return array;
+            }
+            else if (jToken.Type == JTokenType.Array)
+            {
+                // In case of array just use standard mechanism
+                JArray jArray = jToken as JArray;
+
+                return jArray.ToObject(objectType);
+            }
+
+            // In any other case
+            return Array.CreateInstance(_type, 0);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsArray;
+        }
+
+        private JsonReader CopyReaderForObject(JsonReader reader, JObject jObject)
+        {
+            JsonReader jObjectReader = jObject.CreateReader();
+            jObjectReader.Culture = reader.Culture;
+            jObjectReader.DateFormatString = reader.DateFormatString;
+            jObjectReader.DateParseHandling = reader.DateParseHandling;
+            jObjectReader.DateTimeZoneHandling = reader.DateTimeZoneHandling;
+            jObjectReader.FloatParseHandling = reader.FloatParseHandling;
+            jObjectReader.MaxDepth = reader.MaxDepth;
+            jObjectReader.SupportMultipleContent = reader.SupportMultipleContent;
+            return jObjectReader;
+        }
+    }
+}
