62a51e4d25c13d072fb8ef54f801730bd473e6b1
dawid.kraczkowski@htdevelopers.com <dawid.kraczkowski@htdevelopers.com>
2019-11-06 12:00:26 +0100

    ANL-28 Updating patient social templates

--- AthenaHealth.Sdk/Models/Converters/CustomArrayToStringConverter.cs
+++ AthenaHealth.Sdk/Models/Converters/CustomArrayToStringConverter.cs
@@ -11,14 +11,20 @@ namespace AthenaHealth.Sdk.Models.Converters
     public class CustomArrayToStringConverter : JsonConverter
     {
         private readonly string _delimiter;
+        private readonly bool _emptyInsteadOfNull;
 
         public CustomArrayToStringConverter() : this(",")
         {
         }
 
-        public CustomArrayToStringConverter(string delimiter)
+        public CustomArrayToStringConverter(string delimiter) : this(delimiter, false)
+        { 
+        }
+
+        public CustomArrayToStringConverter(string delimiter, bool emptyInsteadOfNull)
         {
             _delimiter = delimiter;
+            _emptyInsteadOfNull = emptyInsteadOfNull;
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
@@ -28,14 +34,16 @@ namespace AthenaHealth.Sdk.Models.Converters
 
         public override bool CanConvert(Type objectType)
         {
-            if (objectType.IsArray)
-                return true;
-            return false;
+            return objectType.IsArray;
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var joinedString = ObjectToStringOrDelimitedStringConverter.Convert(value, _delimiter, false);
+
+            if (_emptyInsteadOfNull && joinedString == null)
+                joinedString = "";
+
             writer.WriteValue(joinedString);
         }
     }
