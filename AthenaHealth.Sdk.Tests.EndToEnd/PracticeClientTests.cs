44cd52679f77b70010ae868aa8217e6b66c254a5
dawid.kraczkowski@htdevelopers.com <dawid.kraczkowski@htdevelopers.com>
2019-11-15 09:12:29 +0100

    ANL-165 Check if has access to practice

--- AthenaHealth.Sdk.Tests.EndToEnd/PracticeClientTests.cs
+++ AthenaHealth.Sdk.Tests.EndToEnd/PracticeClientTests.cs
@@ -3,6 +3,7 @@ using AthenaHealth.Sdk.Models.Request;
 using AthenaHealth.Sdk.Models.Response;
 using AthenaHealth.Sdk.Tests.EndToEnd.Fixtures;
 using Shouldly;
+using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -71,6 +72,32 @@ namespace AthenaHealth.Sdk.Tests.EndToEnd
         public void GetById_NotExistingId_ThrowsApiException()
         {
             Should.Throw<ApiValidationException>(async () => await _client.Practices.GetById(666));
+        }
+
+        [Fact]
+        public async Task HasAccess_ReturnsTrue()
+        {
+            bool response = await _client.Practices.HasAccess(195900);
+
+            response.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void HasAccess_NotExistingPractice_ThrowsException()
+        {
+            ApiValidationException exception = Should.Throw<ApiValidationException>(async () => await _client.Practices.HasAccess(2));
+
+            exception.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+            exception.Message.ShouldContain("The practice ID does not exist.");
+        }
+
+        [Fact]
+        public void HasAccess_UnauthorizedAccess_ThrowsException()
+        {
+            ApiValidationException exception = Should.Throw<ApiValidationException>(async () => await _client.Practices.HasAccess(195901));
+
+            exception.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+            exception.Message.ShouldContain("Incorrect permissions.");
         }
     }
 }
