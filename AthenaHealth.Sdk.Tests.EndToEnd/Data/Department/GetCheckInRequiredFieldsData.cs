263032bd869107c1947f391b57f3ae579abaf9fe
dawid.kraczkowski@htdevelopers.com <dawid.kraczkowski@htdevelopers.com>
2019-11-25 14:24:29 +0100

    ANL-145 Getting required fields

---
+++ AthenaHealth.Sdk.Tests.EndToEnd/Data/Department/GetCheckInRequiredFieldsData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AthenaHealth.Sdk.Tests.EndToEnd.Data.Department
+{
+    public class GetCheckInRequiredFieldsData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1 };
+            yield return new object[] { 102 };
+            //yield return new object[] { 122 };
+            //yield return new object[] { 142 };
+            //yield return new object[] { 144 };
+            //yield return new object[] { 145 };
+            //yield return new object[] { 146 };
+            //yield return new object[] { 148 };
+            //yield return new object[] { 149 };
+            //yield return new object[] { 150 };
+            //yield return new object[] { 155 };
+            //yield return new object[] { 156 };
+            //yield return new object[] { 157 };
+            //yield return new object[] { 158 };
+            //yield return new object[] { 159 };
+            //yield return new object[] { 160 };
+            //yield return new object[] { 161 };
+            //yield return new object[] { 162 };
+            //yield return new object[] { 163 };
+            //yield return new object[] { 165 };
+            //yield return new object[] { 166 };
+            //yield return new object[] { 167 };
+            //yield return new object[] { 168 };
+            //yield return new object[] { 169 };
+            //yield return new object[] { 170 };
+            //yield return new object[] { 171 };
+            //yield return new object[] { 172 };
+            //yield return new object[] { 21 };
+            //yield return new object[] { 22 };
+            //yield return new object[] { 42 };
+            //yield return new object[] { 62 };
+            //yield return new object[] { 82 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ AthenaHealth.Sdk/Models/Response/GetCheckInRequiredFieldsResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace AthenaHealth.Sdk.Models.Response
+{
+    internal class GetCheckInRequiredFieldsResponse
+    {
+        [JsonProperty("fieldlist")]
+        public string[] Fields { get; set; }
+    }
+}
