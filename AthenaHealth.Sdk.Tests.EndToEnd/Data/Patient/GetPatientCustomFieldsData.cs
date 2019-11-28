c422891a01ed050f8f92d7b30decb43150d74844
dawid.kraczkowski@htdevelopers.com <dawid.kraczkowski@htdevelopers.com>
2019-11-28 15:34:20 +0100

    ANL-247 Getting patient custom fields

---
+++ AthenaHealth.Sdk.Tests.EndToEnd/Data/Patient/GetPatientCustomFieldsData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AthenaHealth.Sdk.Tests.EndToEnd.Data.Patient
+{
+    public class GetPatientCustomFieldsData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1 };
+            yield return new object[] { 141 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ AthenaHealth.Sdk/Models/Response/PatientCustomField.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace AthenaHealth.Sdk.Models.Response
+{
+    public class PatientCustomField
+    {
+        /// <summary>
+        /// Corresponds to the /customfields customfieldid.
+        /// </summary>
+        [JsonProperty("customfieldid")]
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// For a non-select custom field, the value.
+        /// </summary>
+        [JsonProperty("customfieldvalue")]
+        public string CustomFieldValue { get; set; }
+
+        /// <summary>
+        /// For a select custom field, the selectid value (from /customfield's selectlist).
+        /// </summary>
+        [JsonProperty("optionid")]
+        public int? OptionId { get; set; }
+    }
+}
